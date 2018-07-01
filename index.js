4f16a1b3eace02f908e2443c1c423cf7cbb282f4
Luca Carducci <luca.carducci91@gmail.com>
2018-07-01 23:38:15 +0200

    obfuscate

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const generateCmd = require('./lib/generate');
 const getProjectUrlCmd = require('./lib/getProjectUrl');
 
 prog
-    .version('1.0.21')
+    .version('1.0.22')
 
     // start
     .command('login', 'Log in into Skaffolder')
