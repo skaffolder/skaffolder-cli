4be46fb04f5dec1411156f6e0980041da3e8ae1b
Luca Carducci <luca.carducci91@gmail.com>
2018-07-16 14:35:59 +0200

    v32

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const generateCmd = require('./lib/generate');
 const getProjectUrlCmd = require('./lib/getProjectUrl');
 
 prog
-    .version('1.0.31')
+    .version('1.0.32')
 
     // start
     .command('login', 'Log in into Skaffolder')
