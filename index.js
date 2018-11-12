df8e7403f5971c3bf6d1a3a1eab1e1690e1f7103
Luca Carducci <luca.carducci91@gmail.com>
2018-11-12 17:13:21 +0100

    1.1.13

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const generateCmd = require('./lib/generate');
 const getProjectUrlCmd = require('./lib/getProjectUrl');
 const importDbCmd = require('./lib/importDb');
 
-prog.version('1.1.12')
+prog.version('1.1.13')
 
     // start
     .command('login', 'Log in into Skaffolder')
