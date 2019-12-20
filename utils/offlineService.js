2fd131ddb6364de87d1f45ff3aee7d282249f174
Andrea Giunio Bruni <andrea.b@skaffolder.com>
2019-12-20 15:04:55 +0100

    fix page sort

--- utils/offlineService.js
+++ utils/offlineService.js
@@ -428,7 +428,7 @@ var translateYamlProject = function (yamlProject) {
 			})
 		})
 	})
-	modules.sort((a, b) => { return a.name > b.name })
+	modules.sort((a, b) => { if ( a.name > b.name ) return 1; if (a.name < b.name) return -1; return 0;})
 
 	skProject.modules = modules;
 
