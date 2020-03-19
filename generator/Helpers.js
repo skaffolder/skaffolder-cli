873e6a4f4e0b436d16403f54ae6540fbe0bdf868
Andrea Giunio Bruni <andrea.b@skaffolder.com>
2020-03-19 13:33:30 +0100

    Fix cannot find module 'extra'

--- generator/Helpers.js
+++ generator/Helpers.js
@@ -1,9 +1,12 @@
 exports.registerHelpers = function(Handlebars) {
-  // Add extra helpers
-  let extra = require(process.cwd() + "/extra");
 
-  for (let i in extra.helpers) {
-    Handlebars.registerHelper(extra.helpers[i].name, extra.helpers[i].fn);
+  // Add extra helpers
+  if (require("fs").existsSync(process.cwd() + "/extra")) {
+    let extra = require(process.cwd() + "/extra");
+
+    for (let i in extra.helpers) {
+      Handlebars.registerHelper(extra.helpers[i].name, extra.helpers[i].fn);
+    }
   }
 
   // Default helpers
